32b4809ac3c6bf59e573462d9d14274a8889f1da
unknown <grzluiz5@gmail.com>
2022-09-17 23:01:11 -0300

    css

--- src/componentes/paginas/Home.js
+++ src/componentes/paginas/Home.js
@@ -1,15 +1,18 @@
 import React from "react"
 import './paginas css/home.css'
+import imgA from "./paginas css/Umirror.gif"
 
 function Home(){
     return(
         <div>
+            <section className="help">
+            <img src={imgA} className="image"/>
             <div className="text">
                 <h1>In terms of male human and female pokemon breeding, for the love of God stay away from Glaceon.</h1>
                 <h4>With its abilities being Snow Cloak and Ice Body, the best thing you would get from it would be severe frostbite. Glaceon is known for being able to lower its body temperature to -75 Fº (-59 Cº), affecting the surroundings and making not only ice crystals out of everything but also turning its fur into fucking ice needles, meaning that only by getting near Glaceon you may get your dick fucking shredded by ice crystals from the inside in your bloodstream as you get an erection. We get it, Glaceon has access to the moves Charm, Attract, Baby-Doll Eyes, etcetera, but let me remind you that any kind of moisture that gets near Glaceon gets frozen almost instantly. To put this in perspective lets imagine you won, you have fucked Glaceon, now you have to deal with the frozen cum icicle forming inside your penis that will end your life without urgent medical atention.</h4>
                 <h3>At this point you deserve death for even thinking it is a good idea to fuck that shit. You know what happens if you manage to stay alive? They have to chop off your dick or you die. Should have picked Vaporeon, fucking dumbass</h3>
             </div>
-            <div className="image"/>
+            </section>
         </div>
     )
 }
