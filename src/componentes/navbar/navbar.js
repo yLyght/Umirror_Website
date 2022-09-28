42e7e281a140342c1607e1b67563a5b0396b6fcf
Enzo Enrico <e.enrico2005@gmail.com>
2022-09-28 09:36:28 -0300

    Interface atualizada

--- src/componentes/navbar/navbar.js
+++ src/componentes/navbar/navbar.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./navbar.css";
 import menutoggle from "../botão/botao"
 
+import logoGif from "./UmirrorLogoGif.gif"
+
 function NavBar (){
     return(
         <nav className="Nav">
             <a href="/">
-            <img height="40" width="50" alt="logo" src="../../unnamed-recovered.jpg" /></a>
+            <img height="40" width="70" src={logoGif} alt="logo"/></a>
             <ul>
                 <div className="toggle" onClick={menutoggle}/>
             </ul>
--- src/componentes/paginas/Home.js
+++ src/componentes/paginas/Home.js
@@ -1,29 +1,36 @@
 import React from "react"
 import './paginas css/home.css'
-import imgA from "./paginas css/Umirror.gif"
-import imgB from "./paginas css/ranmo.png"
 
-function Home(){
-    return(
+
+function Home() {
+    return (
         <div>
             <section className="help">
-            <img src={imgA} alt="gif" className="image"/>
-            <div className="text">
-                <h1>Umirror é um espelho inteligente visado no monitoramento e alivio ou melhoramento do humor de seu usuario</h1>
-                <h4>Nosso  projeto  consiste  em  um  espelho  visado  a  auxiliar  e  monitorar  pessoas  com 
-                distúrbios  e  problemas  comportamentais,  criando  gráficos  e  coletando  dados  para 
-                que o usuário possa monitorar seu estado mental,</h4>
-            </div>
+                {/* <img src={imgA} alt="gif" className="image"/> */}
+                <container>
+                    <div className="text" id="first">
+                        <h1 id="slogan">O espelho que cuida de você</h1>
+                    </div>
+
+                </container>
+                <div className="text" id="second">
+                    <h4>Nosso  projeto  consiste  em  um  espelho  visado  a  auxiliar  e  monitorar  pessoas  com
+                        distúrbios  e  problemas  comportamentais,  criando  gráficos  e  coletando  dados  para:
+                        que o usuário possa monitora seu estado mental,</h4>
+                </div>
             </section>
             <section className="info">
-                <img src={imgB} alt="nobro" className="image2"/>
+                {/* <img src={imgB} alt="nobro" className="image2"/> */}
                 <div className="yeye">
-                    <h4>
-                    usarão  um  algoritmo  que 
-                    usará dados de nosso software de reconhecimento facial para poder dar sugestões, 
-                    tais quais aparecerão em telas de LCD plantadas atrás  do  espelho  e  seu  painel 
-                    refletivo, que  o  usuário  poderá  ver  enquanto  se  arruma  para  seu  dia. 
-                    </h4>
+                    <h1>
+                        Nosso time:
+                        <br/>
+                        <img src={"./paginas css/893.jpg"} alt="penis"/>
+                        <br/>
+                        Twitter
+                        <br/>
+                        Telegram
+                    </h1>
                 </div>
             </section>
         </div>
