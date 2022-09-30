3e6b8d2de7ab1b224c95be57270be3ce345c74ab
unknown <grzluiz5@gmail.com>
2022-09-29 22:19:55 -0300

    bagunça

--- src/componentes/paginas/Home.js
+++ src/componentes/paginas/Home.js
@@ -1,6 +1,6 @@
 import React from "react"
 import './paginas css/home.css'
-
+import imgA from "./paginas css/0001-0110-_1_.gif"
 
 function Home() {
     return (
@@ -19,9 +19,27 @@ function Home() {
                     </h3>
                 </div>
             </section>
+            <section className="yeye">
+                <img src={imgA} alt="gif" className="image"/>
+                <div className="giftext">
+                    <h1>Umirror é um espelho inteligente visado no monitoramento e alivio ou melhoramento do humor de seu usuario</h1>
+                    <h4>Nosso  projeto  consiste  em  um  espelho  visado  a  auxiliar  e  monitorar  pessoas  com 
+                    distúrbios  e  problemas  comportamentais,  criando  gráficos  e  coletando  dados  para 
+                    que o usuário possa monitorar seu estado mental,</h4>
+                </div>
+                <img src="https://via.placeholder.com/2008x2448" alt="nobro" className="image2"/>
+                <div className="place">
+                    <h4>
+                    usarão  um  algoritmo  que 
+                    usará dados de nosso software de reconhecimento facial para poder dar sugestões, 
+                    tais quais aparecerão em telas de LCD plantadas atrás  do  espelho  e  seu  painel 
+                    refletivo, que  o  usuário  poderá  ver  enquanto  se  arruma  para  seu  dia. 
+                    </h4>
+                </div>
+            </section>
             <section className="info">
                 <h1>Nossa Equipe:</h1>
-                <div className="yeye">
+                <div className="cont">
                     <div>
                         <h3>Luiz</h3><h4>,</h4>
                     </div>
